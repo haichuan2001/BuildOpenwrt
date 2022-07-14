47e0bd9e676dcb00ffe00a161cbcef3370d6a3c0
carbon <ecrasy@gmail.com>
2022-07-14 03:19:05 +0000

    update workflow

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+[ -e feeds/luci/collections/luci/Makefile ] && sed -ri 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 #删掉重复
 rm -rf feeds/kenzo/luci-theme-argon feeds/kenzo/luci-theme-argonne
