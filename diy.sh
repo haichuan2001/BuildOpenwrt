3a069807d73e1bcc69d020944743bfb3cb83e08f
carbon <ecrasy@gmail.com>
2022-07-14 04:41:29 +0000

    add dir check to diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 
 # 替换默认theme
 rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+[ -d package/lean ] && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
 [ -e feeds/luci/collections/luci/Makefile ] && sed -ri 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 #删掉重复theme
