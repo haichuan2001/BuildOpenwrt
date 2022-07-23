6eee207732bd982c64e841931f971705e20a13c4
carbon <ecrasy@gmail.com>
2022-07-23 14:12:57 +0000

    try passwall2

--- feeds.sh
+++ feeds.sh
@@ -3,7 +3,7 @@
 # Author: Carbon (ecras_y@163.com)
 # Description: feel free to use
 # Created Time: 2022-07-23 13:04:43 UTC
-# Modified Time: 2022-07-23 13:04:49 UTC
+# Modified Time: 2022-07-23 14:11:53 UTC
 #########################################################################
 
 #!/bin/bash
@@ -14,8 +14,11 @@ echo "Adding xiaorouji passwall"
 echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 
+# passwall2
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+
 # ssrp
 echo "Adding ShadowSocksR Plus+"
-echo "src-git ssrp https://github.com/fw876/helloworld.git" >> feeds.conf.default
+echo "src-git ssrp https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
 
 echo "Add Feeds Completed!!!"
