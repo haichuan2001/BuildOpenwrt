558b86a812b7b4b11c9edb7f2e1890475807f230
carbon <ecrasy@gmail.com>
2022-07-25 01:19:21 +0000

    update configs

--- data/model.sh
+++ data/model.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sed -i 's/default//Ig' /tmp/sysinfo/model
-sed -i 's/string//Ig' /tmp/sysinfo/model
-sed -i 's/^[ \t]*//g' /tmp/sysinfo/model
-sed -i 's/[ \t]*$//g' /tmp/sysinfo/model
+model="Default string Default string/Default string - "
+replace=""
+command=s@$model@$replace@
+sed -i "$command" /tmp/sysinfo/model
+
