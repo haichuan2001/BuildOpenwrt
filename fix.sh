efa89fdfcc4ec6d29a768741282ffde61d281f05
carbon <ecrasy@gmail.com>
2022-07-30 12:29:04 +0000

    update fix sh

--- fix.sh
+++ fix.sh
@@ -3,14 +3,14 @@
 # Author: Carbon (ecras_y@163.com)
 # Description: feel free to use
 # Created Time: 2022-07-30 04:57:44 UTC
-# Modified Time: 2022-07-30 04:59:42 UTC
+# Modified Time: 2022-07-30 12:25:37 UTC
 #########################################################################
 
 
 #!/bin/bash
 
 # 修正cjdns编译错误
-sed -i 's/-Wno-error=stringop-overflow//g' package/feeds/routing/cjdns/Makefile
-sed -i 's/-Wno-error=stringop-overread//g' package/feeds/routing/cjdns/Makefile
+sed -i 's/ -Wno-error=stringop-overflow//g' package/feeds/routing/cjdns/Makefile
+sed -i 's/ -Wno-error=stringop-overread//g' package/feeds/routing/cjdns/Makefile
 
 echo "Fixing cjdns makefile error!!!"
