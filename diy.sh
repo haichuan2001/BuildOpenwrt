87189b6854355f0fe9abe6c8870562f5e203db5c
carbon <ecrasy@gmail.com>
2022-07-30 05:00:40 +0000

    update diy sh

--- diy.sh
+++ diy.sh
@@ -3,7 +3,7 @@
 # Author: Carbon (ecras_y@163.com)
 # Description: feel free to use
 # Created Time: 2022-07-23 13:01:29 UTC
-# Modified Time: 2022-07-30 02:05:35 UTC
+# Modified Time: 2022-07-30 05:00:27 UTC
 #########################################################################
 
 
@@ -35,9 +35,5 @@ chmod +x package/base-files/files/etc/hotplug.d/iface/99-ipv6
 
 # 替换默认theme
 # [ -e feeds/luci/collections/luci/Makefile ] && sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 删掉煞笔库
-# 煞笔们不要再引用这智障煞笔库了
-# rm -rf package/libs/mbedtls
 
 echo "DIY Completed!!!"
